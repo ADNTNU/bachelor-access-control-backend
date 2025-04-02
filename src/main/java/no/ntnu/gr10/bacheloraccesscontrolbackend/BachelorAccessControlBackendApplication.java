947e55ec6a45d589275e9f7e7782721f9e7e9143
Anders Lund <61545005+AnderzL7@users.noreply.github.com>
2025-04-02 19:12:12 +0200

    Create initial Spring boot project

---
+++ src/main/java/no/ntnu/gr10/bacheloraccesscontrolbackend/BachelorAccessControlBackendApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr10.bacheloraccesscontrolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorAccessControlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BachelorAccessControlBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/ntnu/gr10/bacheloraccesscontrolbackend/BachelorAccessControlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr10.bacheloraccesscontrolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorAccessControlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
